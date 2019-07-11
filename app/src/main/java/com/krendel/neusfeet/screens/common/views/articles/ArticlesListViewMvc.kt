94c9a19329a152d49954d39a9fc0389cd68114d0
Dmitry Golovko <golovko.d.i@gmail.com>
2019-07-11 21:01:50 +0300

    fixed empty in articles

--- app/src/main/java/com/krendel/neusfeet/screens/common/views/articles/ArticlesListViewMvc.kt
+++ app/src/main/java/com/krendel/neusfeet/screens/common/views/articles/ArticlesListViewMvc.kt
@@ -2,9 +2,10 @@ package com.krendel.neusfeet.screens.common.views.articles
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.core.view.doOnPreDraw
+import android.widget.ViewSwitcher
 import androidx.databinding.ObservableField
 import androidx.paging.PagedList
+import androidx.recyclerview.widget.RecyclerView
 import com.krendel.neusfeet.R
 import com.krendel.neusfeet.databinding.ViewArticlesListBinding
 import com.krendel.neusfeet.model.Article
@@ -30,7 +31,9 @@ class ArticlesListViewMvc(
 
     init {
         dataBinding.recyclerView.addItemDecoration(ArticlesItemsDecorator())
-        dataBinding.recyclerView.adapter = RecyclerPagingBindingAdapter(null, listEventsObserver)
+        val adapter = RecyclerPagingBindingAdapter(null, listEventsObserver)
+        dataBinding.recyclerView.adapter = adapter
+        adapter.registerAdapterDataObserver(AdapterObserver(dataBinding.viewSwitcher, adapter))
         dataBinding.refreshLayout.setOnRefreshListener {
             sendEvent(ArticlesListActions.Refresh)
         }
@@ -52,19 +55,34 @@ class ArticlesListViewMvc(
     }
 
     fun setArticles(articles: PagedList<ArticleItemViewModel>) {
-        dataBinding.viewSwitcher.displayedChild = 1
+        // TODO callback for list size?
+//        dataBinding.viewSwitcher.displayedChild = 1
 //        dataBinding.recyclerView.doOnPreDraw {
 //            dataBinding.viewSwitcher.displayedChild = if (articles.isEmpty()) 0 else 1
 //        }
         dataBinding.refreshLayout.isRefreshing = false
 
-        if (recyclerData.get() == null) {
-            recyclerData.set(articles)
-        }
+        // TODO need to think of how to update data list. before invalidation list can be updated on it's own, after - needs to be changed with a new one
+//        if (recyclerData.get() == null) {
+        recyclerData.set(articles)
+//        }
     }
 }
 
 sealed class ArticlesListActions : ViewMvcActions {
     data class ArticleClicked(val article: Article) : ArticlesListActions()
     object Refresh : ArticlesListActions()
+}
+
+private class AdapterObserver(
+    private val viewSwitcher: ViewSwitcher,
+    private val adapter: RecyclerView.Adapter<*>
+) : RecyclerView.AdapterDataObserver() {
+    override fun onChanged() {
+//        viewSwitcher.displayedChild = if (adapter.itemCount > 0) 0 else 1
+    }
+
+    override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
+        viewSwitcher.displayedChild = if (adapter.itemCount == 0) 0 else 1
+    }
 }
